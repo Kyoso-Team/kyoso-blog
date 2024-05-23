fb433a58a43168fb79606d7ea5351cbe116aa42c
Entropy <45132530+Entropy-10@users.noreply.github.com>
2024-05-22 22:36:14 -0700

    bump deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import svelte from '@astrojs/svelte';
-import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [svelte(), tailwind()]
